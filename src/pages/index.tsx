2f929d91d46f620b3f0722abf7c6d8d21ed6aed4
szvitek <gy.szvitek@gmail.com>
2023-10-12 15:07:41 +0100

    feat(NewTweet): New tweet form and components created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import NewTweetForm from "~/components/NewTweetForm";
+
 export default function Home() {
   return (
     <>
-      <h1>Hi</h1>
+      <header className="sticky top-0 z-10 border-b bg-white pt-2">
+        <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+      </header>
+      <NewTweetForm />
     </>
   );
 }
