afee0a6c70c84ed2bab119213c509c9418594536
szvitek <gy.szvitek@gmail.com>
2023-10-18 17:42:08 +0100

    feat(Profile): profile page server logic

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,6 +1,62 @@
-const Profiles = () => {
+import type {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  NextPage,
+} from "next";
+import Head from "next/head";
+import { ssgHelper } from "~/server/api/ssgHelper";
+import { api } from "~/utils/api";
+import ErrorPage from "next/error";
+
+const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  id,
+}) => {
+  const { data: profile } = api.profile.getById.useQuery({ id });
+
+  if (profile?.name == null) return <ErrorPage statusCode={404} />;
+
   return (
-    <div>Profiles</div>
-  )
+    <>
+      <Head>
+        <title>{`Twitter Clone - ${profile.name}`}</title>
+      </Head>
+      {profile.name}
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [], // paths needs to be generated
+    fallback: "blocking", // what to do if page not found in paths
+  };
+};
+
+// this one allows static site generation
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>,
+) {
+  const id = context.params?.id;
+
+  if (id == null) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  const ssg = ssgHelper();
+  // need to prefetch here for ssg sites
+  await ssg.profile.getById.prefetch({ id });
+
+  return {
+    props: {
+      id,
+      trpcState: ssg.dehydrate(),
+    },
+  };
 }
-export default Profiles
+
+export default ProfilePage;
