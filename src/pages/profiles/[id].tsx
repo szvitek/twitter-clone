8c88c22e2b479546145d67ca2574fe74c81b20c3
szvitek <gy.szvitek@gmail.com>
2023-10-18 18:54:02 +0100

    feat(Profile): Profile page design

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -8,23 +8,90 @@ import Head from "next/head";
 import { ssgHelper } from "~/server/api/ssgHelper";
 import { api } from "~/utils/api";
 import ErrorPage from "next/error";
+import Link from "next/link";
+import IconHoverEffect from "~/components/IconHoverEffect";
+import { VscArrowLeft } from "react-icons/vsc";
+import ProfileImage from "~/components/ProfileImage";
+import InfiniteTweetList from "~/components/InfiniteTweetList";
+import { useSession } from "next-auth/react";
+import Button from "~/components/Button";
+
+const pluralRules = new Intl.PluralRules();
+function getPlural(number: number, singular: string, plural: string) {
+  return pluralRules.select(number) === "one" ? singular : plural;
+}
 
 const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   id,
 }) => {
   const { data: profile } = api.profile.getById.useQuery({ id });
+  const tweets = api.tweet.infiniteProfileFeed.useInfiniteQuery(
+    { userId: id },
+    { getNextPageParam: (lastpage) => lastpage.nextCursor },
+  );
 
-  if (profile?.name == null) return <ErrorPage statusCode={404} />;
+  if (profile?.name == null) {
+    return <ErrorPage statusCode={404} />;
+  }
 
   return (
     <>
       <Head>
         <title>{`Twitter Clone - ${profile.name}`}</title>
       </Head>
-      {profile.name}
+      <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <Link href=".." className="mr-2">
+          <IconHoverEffect>
+            <VscArrowLeft className="h-6 w-6" />
+          </IconHoverEffect>
+        </Link>
+        <ProfileImage src={profile.image} className="flex-shrink-0" />
+        <div className="ml-2 flex-grow">
+          <h1 className="text-lg font-bold">{profile.name}</h1>
+          <div className="text-gray-500">
+            {profile.tweetsCount}{" "}
+            {getPlural(profile.tweetsCount, "Tweet", "Tweets")} -{" "}
+            {profile.followersCount}{" "}
+            {getPlural(profile.followersCount, "Follower", "Followers")} -{" "}
+            {profile.followsCount} Following
+          </div>
+        </div>
+        <FollowButton
+          isFollowing={profile.isFollowing}
+          userId={id}
+          onClick={() => null}
+        />
+      </header>
+      <main>
+        <InfiniteTweetList
+          tweets={tweets?.data?.pages.flatMap((page) => page.tweets)}
+          isError={tweets.isError}
+          isLoading={tweets.isLoading}
+          hasMore={tweets.hasNextPage}
+          fetchNewTweets={tweets.fetchNextPage}
+        />
+      </main>
     </>
   );
 };
+
+type FollowButtonProps = {
+  userId: string;
+  isFollowing: boolean
+  onClick: () => void
+};
+
+function FollowButton({ userId, isFollowing, onClick }: FollowButtonProps) {
+  const session = useSession()
+
+  if (session.status !== "authenticated" || session.data.user.id === userId) {
+    return null;
+  }
+
+  return <Button onClick={onClick} small gray={isFollowing}>
+    {isFollowing ? "Unfollow" : "Follow"}
+  </Button>
+}
 
 export const getStaticPaths: GetStaticPaths = () => {
   return {
